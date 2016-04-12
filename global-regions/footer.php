7c630faf2ba83c89192a3c584877506ddb9e2601
leighsapir <shia@incsub.com>
2016-04-11 23:32:49 -0500

    [Footer] Create global region

---
+++ global-regions/footer.php
@@ -0,0 +1,48 @@
+<?php
+/* START_REGION_OUTPUT */
+$footer = upfront_create_region(
+			array (
+  'name' => 'footer',
+  'title' => 'Footer',
+  'type' => 'clip',
+  'scope' => 'global',
+  'container' => 'footer',
+  'position' => 10,
+  'allow_sidebar' => true,
+),
+			array (
+  'version' => '1.0.0',
+  'row' => 8,
+  'breakpoint' => 
+  (array)(array(
+     'tablet' => 
+    (array)(array(
+       'edited' => false,
+       'col' => 24,
+    )),
+     'mobile' => 
+    (array)(array(
+       'edited' => false,
+       'col' => 24,
+    )),
+  )),
+  'background_type' => 'color',
+  'use_padding' => 0,
+  'sub_regions' => 
+  array (
+    0 => false,
+  ),
+  'bg_padding_type' => 'varied',
+  'top_bg_padding_slider' => 0,
+  'top_bg_padding_num' => 0,
+  'bottom_bg_padding_slider' => 0,
+  'bottom_bg_padding_num' => 0,
+  'bg_padding_slider' => 0,
+  'bg_padding_num' => 0,
+  'background_color' => '#ufc0',
+)
+			);
+
+$regions->add($footer);
+
+/* END_REGION_OUTPUT */
