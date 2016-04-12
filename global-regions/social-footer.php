8ec74117da31bbe99a6ba2f44bcbcff6c3efafe7
leighsapir <shia@incsub.com>
2016-04-11 23:33:00 -0500

    [Social Footer] Create global region

---
+++ global-regions/social-footer.php
@@ -0,0 +1,48 @@
+<?php
+/* START_REGION_OUTPUT */
+$social_footer = upfront_create_region(
+			array (
+  'name' => 'social-footer',
+  'title' => 'Social Footer',
+  'type' => 'clip',
+  'scope' => 'global',
+  'container' => 'social-footer',
+  'position' => 10,
+  'allow_sidebar' => true,
+),
+			array (
+  'version' => '1.0.0',
+  'row' => 53,
+  'breakpoint' => 
+  (array)(array(
+     'tablet' => 
+    (array)(array(
+       'edited' => false,
+       'col' => 24,
+    )),
+     'mobile' => 
+    (array)(array(
+       'edited' => false,
+       'col' => 24,
+    )),
+  )),
+  'background_type' => 'color',
+  'use_padding' => 0,
+  'sub_regions' => 
+  array (
+    0 => false,
+  ),
+  'bg_padding_type' => 'varied',
+  'top_bg_padding_slider' => 0,
+  'top_bg_padding_num' => 0,
+  'bottom_bg_padding_slider' => 0,
+  'bottom_bg_padding_num' => 0,
+  'bg_padding_slider' => 0,
+  'bg_padding_num' => 0,
+  'background_color' => '#ufc3',
+)
+			);
+
+$regions->add($social_footer);
+
+/* END_REGION_OUTPUT */
