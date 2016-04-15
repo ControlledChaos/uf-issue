a9c8907d0ebee589b6e901e77b3eea9b187c70b4
leighsapir <shia@incsub.com>
2016-04-14 21:47:02 -0500

    [404 Page] Update content

--- layouts/single-404_page.php
+++ layouts/single-404_page.php
@@ -1,37 +1,363 @@
 <?php
+$layout_version = '1.0.0';
+
 if (file_exists(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'header.php')) include(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'header.php');
 
-$main = upfront_create_region(array(
-	"name" => "main",
-	"title" => __("Main Area"),
-	"scope" => "local",
-	"type" => "wide",
-	"default" => true,
-	"allow_sidebar" => true
-), array(
-	"row" => 140,
-	"background_type" => "color",
-	"background_color" => "#c5d0db"
+$main = upfront_create_region(
+			array (
+  'name' => 'main',
+  'title' => 'Main Area',
+  'type' => 'clip',
+  'scope' => 'local',
+  'container' => 'main',
+  'position' => 10,
+  'allow_sidebar' => true,
+),
+			array (
+  'row' => 100,
+  'background_type' => 'color',
+  'background_color' => '#ufc6',
+  'version' => '1.0.0',
+  'breakpoint' => 
+  (array)(array(
+     'tablet' => 
+    (array)(array(
+       'edited' => false,
+       'col' => 24,
+    )),
+     'mobile' => 
+    (array)(array(
+       'edited' => false,
+       'col' => 24,
+    )),
+  )),
+  'use_padding' => 0,
+  'sub_regions' => 
+  array (
+    0 => false,
+  ),
+  'bg_padding_type' => 'varied',
+  'top_bg_padding_slider' => '60',
+  'top_bg_padding_num' => '60',
+  'bottom_bg_padding_slider' => '60',
+  'bottom_bg_padding_num' => '60',
+  'bg_padding_slider' => 0,
+  'bg_padding_num' => 0,
+)
+			);
+
+$main->add_element("Uspacer", array (
+  'columns' => '5',
+  'class' => 'upfront-module-spacer',
+  'id' => 'module-1460687902587-1423',
+  'options' => 
+  array (
+    'type' => 'UspacerModel',
+    'view_class' => 'UspacerView',
+    'element_id' => 'spacer-object-1460687902587-1813',
+    'class' => 'c24 upfront-object-spacer',
+    'has_settings' => 0,
+    'id_slug' => 'uspacer',
+  ),
+  'wrapper_id' => 'wrapper-1460687902585-1706',
+  'default_hide' => 1,
+  'toggle_hide' => 0,
+  'hide' => 0,
+  'edited' => true,
+  'new_line' => true,
 ));
 
-$main->add_element("PlainTxt", array(
-	"columns" => "24",
-	"margin_left" => "0",
-	"margin_right" => "0",
-	"margin_top" => "6",
-	"margin_bottom" => "0",
-	"id" => "default-nav-text-module",
-	"rows" => 12,
-	"options" => array(
-		"view_class" => "PlainTxtView",
-		"id_slug" => "plaintxt",
-		"content" => "<p>Uh oh, somehting went wrong!</p>",
-		"element_id" => "default-nav-text-object",
-		"class" => "c24",
-		"type" => "PlainTxtModel",
-		"has_settings" => 1
-	)
+$main->add_element("Uimage", array (
+  'columns' => '8',
+  'margin_left' => '0',
+  'margin_top' => '0',
+  'class' => 'upfront-image_module',
+  'id' => 'module-1460686607272-1042',
+  'options' => 
+  array (
+    'src' => '{{upfront:style_url}}/images/single-404_page/issue-oops-268x281-4429.png',
+    'srcFull' => '{{upfront:style_url}}/images/single-404_page/issue-oops.png',
+    'srcOriginal' => '{{upfront:style_url}}/images/single-404_page/issue-oops.png',
+    'image_title' => '',
+    'alternative_text' => '',
+    'include_image_caption' => true,
+    'image_caption' => '<p>My awesome image caption</p>',
+    'caption_position' => false,
+    'caption_alignment' => false,
+    'caption_trigger' => 'always_show',
+    'image_status' => 'ok',
+    'size' => 
+    (array)(array(
+       'width' => 268,
+       'height' => 281,
+    )),
+    'fullSize' => 
+    (array)(array(
+       'width' => 268,
+       'height' => 281,
+       'top' => 120,
+       'left' => 445,
+    )),
+    'position' => 
+    (array)(array(
+       'top' => 0,
+       'left' => 0,
+    )),
+    'marginTop' => 0,
+    'element_size' => 
+    (array)(array(
+       'width' => 360,
+       'height' => 285,
+    )),
+    'rotation' => 0,
+    'color' => '#ffffff',
+    'background' => '#000000',
+    'captionBackground' => '0',
+    'image_id' => '213',
+    'align' => 'center',
+    'stretch' => false,
+    'vstretch' => false,
+    'quick_swap' => false,
+    'is_locked' => true,
+    'gifImage' => 0,
+    'placeholder_class' => '',
+    'preset' => 'default',
+    'display_caption' => 'showCaption',
+    'type' => 'UimageModel',
+    'view_class' => 'UimageView',
+    'has_settings' => 1,
+    'class' => 'upfront-image',
+    'id_slug' => 'image',
+    'when_clicked' => false,
+    'image_link' => '',
+    'link' => 
+    (array)(array(
+       'type' => false,
+       'url' => '',
+       'target' => false,
+    )),
+    'element_id' => 'image-1460686607267-1040',
+    'padding_slider' => '15',
+    'top_padding_num' => '15',
+    'bottom_padding_num' => '15',
+    'use_padding' => 'yes',
+    'usingNewAppearance' => true,
+    'valign' => 'center',
+    'isDotAlign' => false,
+    'row' => 57,
+  ),
+  'row' => 57,
+  'wrapper_id' => 'wrapper-1460686952358-1072',
+  'edited' => true,
+  'wrapper_breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'col' => 12,
+      'order' => 1,
+      'clear' => true,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'col' => 7,
+      'order' => 1,
+      'clear' => true,
+    ),
+  ),
+  'breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 12,
+      'order' => 0,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 7,
+      'order' => 0,
+    ),
+  ),
+));
+
+$main->add_element("PlainTxt", array (
+  'columns' => '6',
+  'margin_left' => '0',
+  'margin_right' => '0',
+  'margin_top' => '0',
+  'margin_bottom' => '0',
+  'class' => 'default-nav-text-module',
+  'id' => 'default-nav-text-module',
+  'options' => 
+  array (
+    'view_class' => 'PlainTxtView',
+    'id_slug' => 'plaintxt',
+    'content' => '<h1 class="">404 PAGE NOT FOUND</h1>',
+    'element_id' => 'default-nav-text-object',
+    'class' => 'c24',
+    'type' => 'PlainTxtModel',
+    'has_settings' => 1,
+    'top_padding_use' => 'yes',
+    'top_padding_num' => '60',
+    'preset' => 'textbox-alternative',
+    'padding_slider' => '15',
+    'bottom_padding_num' => '15',
+    'use_padding' => 'yes',
+    'is_edited' => true,
+    'lock_padding' => '',
+    'top_padding_slider' => '60',
+    'row' => 17,
+    'padding_number' => '15',
+    'left_padding_num' => '15',
+    'right_padding_num' => '15',
+    'anchor' => '',
+    'usingNewAppearance' => true,
+    'theme_style' => '',
+    'breakpoint_presets' => 
+    (array)(array(
+       'desktop' => 
+      (array)(array(
+         'preset' => 'textbox-alternative',
+      )),
+    )),
+    'bottom_padding_use' => 'yes',
+    'bottom_padding_slider' => '15',
+    'right_padding_use' => 'yes',
+  ),
+  'row' => 17,
+  'sticky' => false,
+  'default_hide' => 0,
+  'hide' => 0,
+  'toggle_hide' => 1,
+  'wrapper_id' => 'wrapper-1460687470083-1251',
+  'edited' => true,
+  'wrapper_breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'col' => 6,
+      'order' => 2,
+      'clear' => true,
+    ),
+    'mobile' => 
+    array (
+      'col' => 7,
+      'order' => 2,
+      'clear' => true,
+    ),
+  ),
+  'breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'col' => 6,
+      'left' => 0,
+      'top' => 0,
+      'order' => 0,
+    ),
+    'mobile' => 
+    array (
+      'col' => 7,
+      'left' => 0,
+      'top' => 0,
+      'order' => 0,
+    ),
+  ),
+  'close_wrapper' => false,
+));
+
+$main->add_element("Uwidget", array (
+  'columns' => '6',
+  'margin_left' => '0',
+  'margin_top' => '0',
+  'class' => 'upfront-widget_module',
+  'id' => 'module-1460686607293-1293',
+  'options' => 
+  array (
+    'id_slug' => 'uwidget',
+    'type' => 'UwidgetModel',
+    'view_class' => 'UwidgetView',
+    'class' => 'c24 upfront-widget',
+    'has_settings' => 1,
+    'preset' => 'default',
+    'widget' => 'search-2',
+    'element_id' => 'uwidget-object-1460686607292-1056',
+    'current_widget' => 'search-2',
+    'current_widget_specific_settings' => false,
+    'padding_slider' => '15',
+    'top_padding_num' => '15',
+    'bottom_padding_num' => '15',
+    'use_padding' => 'yes',
+    'usingNewAppearance' => true,
+    'lock_padding' => '',
+    'padding_number' => '15',
+    'left_padding_num' => '15',
+    'right_padding_num' => '15',
+    'anchor' => '',
+    'widget_specific_fields' => false,
+    'current_widget_specific_fields' => 
+    (array)(array(
+       'widget-search-__i__-title' => 
+      (array)(array(
+         'label' => 'Title: ',
+         'name' => 'title',
+         'type' => 'text',
+         'value' => '',
+      )),
+    )),
+    'title' => '',
+    'row' => 11,
+  ),
+  'row' => 11,
+  'wrapper_id' => 'wrapper-1460687470083-1251',
+  'edited' => true,
+  'breakpoint' => 
+  array (
+    'tablet' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 6,
+      'order' => 1,
+    ),
+    'mobile' => 
+    array (
+      'edited' => false,
+      'left' => 0,
+      'col' => 7,
+      'order' => 0,
+    ),
+  ),
+));
+
+$main->add_element("Uspacer", array (
+  'columns' => '5',
+  'class' => 'upfront-module-spacer',
+  'id' => 'module-1460687479005-1792',
+  'options' => 
+  array (
+    'type' => 'UspacerModel',
+    'view_class' => 'UspacerView',
+    'element_id' => 'spacer-object-1460687479005-1471',
+    'class' => 'c24 upfront-object-spacer',
+    'has_settings' => 0,
+    'id_slug' => 'uspacer',
+  ),
+  'wrapper_id' => 'wrapper-1460687479003-1075',
+  'default_hide' => 1,
+  'toggle_hide' => 0,
+  'hide' => 0,
+  'edited' => true,
 ));
 
 $regions->add($main);
+
+if (file_exists(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'social-footer.php')) include(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'social-footer.php');
+
 if (file_exists(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'footer.php')) include(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'footer.php');
+
