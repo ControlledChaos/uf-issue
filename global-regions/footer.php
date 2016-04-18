809c70d386b1808401b96635b3cc8e2cf49f6754
leighsapir <shia@incsub.com>
2016-04-18 16:00:42 -0500

    [Global Region] Update

--- global-regions/footer.php
+++ global-regions/footer.php
@@ -99,13 +99,13 @@ $footer->add_element("Unewnavigation", array (
     array (
       0 => 
       (array)(array(
-         'menu-item-db-id' => 287,
-         'menu-item-parent-id' => 0,
+         'menu-item-db-id' => 319,
+         'menu-item-parent-id' => '0',
          'menu-item-type' => 'custom',
-         'menu-item-title' => 'ARCHIVE',
+         'menu-item-title' => 'ADVERTISE',
          'menu-item-url' => '#',
          'menu-item-object' => 'custom',
-         'menu-item-object-id' => '287',
+         'menu-item-object-id' => '319',
          'menu-item-target' => '',
          'menu-item-position' => 1,
          'link' => 
@@ -117,103 +117,13 @@ $footer->add_element("Unewnavigation", array (
       )),
       1 => 
       (array)(array(
-         'menu-item-db-id' => 286,
-         'menu-item-parent-id' => 0,
-         'menu-item-type' => 'custom',
-         'menu-item-title' => 'ARCHIVE',
-         'menu-item-url' => '#',
-         'menu-item-object' => 'custom',
-         'menu-item-object-id' => '286',
-         'menu-item-target' => '',
-         'menu-item-position' => 0,
-         'link' => 
-        (array)(array(
-           'type' => 'unlink',
-           'url' => '#',
-           'target' => '',
-        )),
-      )),
-      2 => 
-      (array)(array(
-         'menu-item-db-id' => 291,
-         'menu-item-parent-id' => 0,
-         'menu-item-type' => 'custom',
-         'menu-item-title' => 'ADVERTISE',
-         'menu-item-url' => '#',
-         'menu-item-object' => 'custom',
-         'menu-item-object-id' => '291',
-         'menu-item-target' => '',
-         'menu-item-position' => 1,
-         'link' => 
-        (array)(array(
-           'type' => 'unlink',
-           'url' => '#',
-           'target' => '',
-        )),
-      )),
-      3 => 
-      (array)(array(
-         'menu-item-db-id' => 288,
-         'menu-item-parent-id' => 0,
-         'menu-item-type' => 'custom',
-         'menu-item-title' => 'ADVERTISE',
-         'menu-item-url' => '#',
-         'menu-item-object' => 'custom',
-         'menu-item-object-id' => '288',
-         'menu-item-target' => '',
-         'menu-item-position' => 0,
-         'link' => 
-        (array)(array(
-           'type' => 'unlink',
-           'url' => '#',
-           'target' => '',
-        )),
-      )),
-      4 => 
-      (array)(array(
-         'menu-item-db-id' => 294,
-         'menu-item-parent-id' => 0,
+         'menu-item-db-id' => 320,
+         'menu-item-parent-id' => '0',
          'menu-item-type' => 'custom',
          'menu-item-title' => 'CONTACT',
          'menu-item-url' => '#',
          'menu-item-object' => 'custom',
-         'menu-item-object-id' => '294',
-         'menu-item-target' => '',
-         'menu-item-position' => 1,
-         'link' => 
-        (array)(array(
-           'type' => 'unlink',
-           'url' => '#',
-           'target' => '',
-        )),
-      )),
-      5 => 
-      (array)(array(
-         'menu-item-db-id' => 293,
-         'menu-item-parent-id' => 0,
-         'menu-item-type' => 'custom',
-         'menu-item-title' => 'CONTACT',
-         'menu-item-url' => '#',
-         'menu-item-object' => 'custom',
-         'menu-item-object-id' => '293',
-         'menu-item-target' => '',
-         'menu-item-position' => 3,
-         'link' => 
-        (array)(array(
-           'type' => 'unlink',
-           'url' => '#',
-           'target' => '',
-        )),
-      )),
-      6 => 
-      (array)(array(
-         'menu-item-db-id' => 296,
-         'menu-item-parent-id' => 0,
-         'menu-item-type' => 'custom',
-         'menu-item-title' => 'TERMS OF USE',
-         'menu-item-url' => '#',
-         'menu-item-object' => 'custom',
-         'menu-item-object-id' => '296',
+         'menu-item-object-id' => '320',
          'menu-item-target' => '',
          'menu-item-position' => 2,
          'link' => 
@@ -223,17 +133,17 @@ $footer->add_element("Unewnavigation", array (
            'target' => '',
         )),
       )),
-      7 => 
+      2 => 
       (array)(array(
-         'menu-item-db-id' => 295,
-         'menu-item-parent-id' => 0,
+         'menu-item-db-id' => 321,
+         'menu-item-parent-id' => '0',
          'menu-item-type' => 'custom',
          'menu-item-title' => 'TERMS OF USE',
          'menu-item-url' => '#',
          'menu-item-object' => 'custom',
-         'menu-item-object-id' => '295',
+         'menu-item-object-id' => '321',
          'menu-item-target' => '',
-         'menu-item-position' => 4,
+         'menu-item-position' => 3,
          'link' => 
         (array)(array(
            'type' => 'unlink',
